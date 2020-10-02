a98f5f5caf614f870a98fb03a2f2b18c79587874
klimentinalogan <45449009+klimentinalogan@users.noreply.github.com>
2020-10-02 12:28:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 function max(numbers) {
     var max = numbers[0];
+    if(numbers = []) return 0;
     for (i = 0; i < numbers.length; i++) {
         if ( max < numbers[i]) max = numbers[i];
     }
@@ -8,6 +9,7 @@ function max(numbers) {
 
 function min(numbers) {
     var min = numbers[0];
+    if(numbers = []) return 0;
     for (i = 0; i < numbers.length; i++) {
         if ( min > numbers[i]) min = numbers[i];
     }
@@ -16,6 +18,7 @@ function min(numbers) {
 
 function avg (numbers) {
     sum = 0;
+    if(numbers = []) return 0;
     for (i = 0; i < numbers.length; i++) {
         sum = sum + numbers[i];
     }
