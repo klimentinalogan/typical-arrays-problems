533dbbf9db56842cb891d67d7a4bc17dfef85005
klimentinalogan <45449009+klimentinalogan@users.noreply.github.com>
2020-10-03 21:15:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,4 @@
 exports.min = function min (array) {
-    return 0;
-}
-
-exports.max = function max (array) {
-    return 0;
-}
-
-exports.avg = function avg (array) {
-    return 0;
-}
-
-function max(array) {
-    var max = array[0];
-    for (i = 0; i < array.length; i++) {
-        if ( max < array[i]) max = array[i];
-    }
-    return max;
-}
-
-function min(array) {
     var min = array[0];
     for (i = 0; i < array.length; i++) {
         if ( min > array[i]) min = array[i];
@@ -26,11 +6,20 @@ function min(array) {
     return min;
 }
 
-function avg(array) {
+exports.max = function max (array) {
+   var max = array[0];
+    for (i = 0; i < array.length; i++) {
+        if ( max < array[i]) max = array[i];
+    }
+    return max;
+}
+
+exports.avg = function avg (array) {
     sum = 0;
     for (i = 0; i < array.length; i++) {
         sum = sum + array[i];
     }
     return sum / array.length;
 }
+
 
