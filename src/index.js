b448bcdac1e24d7c2b2c23f073715f6c732d91eb
klimentinalogan <45449009+klimentinalogan@users.noreply.github.com>
2020-10-03 20:23:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 exports.min = function min (array) {
+    return 0;
     let min = array[0];
     if(numbers = []) return 0;
     for (i = 0; i < numbers.length; i++) {
         if ( min > numbers[i]) min = numbers[i];
     }
     return min;
-    return 0;
 }
 
 exports.max = function max (array) {
+    return 0;
     let max = array[0];
     if(numbers = []) return 0;
     for (i = 0; i < numbers.length; i++) {
         if ( max < numbers[i]) max = numbers[i];
     }
     return max;
-    return 0;
 }
 
 exports.avg = function avg (array) {
+    return 0;
     sum = 0;
     if(numbers = []) return 0;
     for (i = 0; i < numbers.length; i++) {
         sum = sum + numbers[i];
     }
     return sum / numbers.length;
-    return 0;
 }
 
 
